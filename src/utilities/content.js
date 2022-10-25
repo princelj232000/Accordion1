b401a4962a09c9b5402fe25e9fe00f60fd128ab8
Lhouie Jay Canoy <lhouiejay.canoy.21@usjr.edu.ph>
2022-10-25 21:48:03 +0800

    adding utility folder and content.js

---
+++ src/utilities/content.js
@@ -0,0 +1,15 @@
+export const accordionData = [
+    {
+      title: 'Albert Einstein',
+      content: `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”
+      ― Albert Einstein`
+    },
+    {
+      title: 'Isaac Newton',
+      content: `“I seem to have been only like a boy playing on the seashore, and diverting myself in now and then finding a smoother pebble or a prettier shell than ordinary, whilst the great ocean of truth lay all undiscovered before me.” – Isaac Newton `
+    },
+    {
+      title: 'Brian Greene',
+      content: `“Science is a way of life. It is a perspective. Science is the process that takes us from confusion to understanding in a manner that’s precise, predictive, and reliable – a transformation, for those lucky enough to experience it, that is empowering and emotional.” – Brian Greene `
+    }
+  ];
